247ee2a79502d1355c62ea7114867835cf400548
Nidhiparikh <nidhiparikh711@gmail.com>
2025-01-10 11:26:50 +0530

    react portfolio

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Nidhi Parikh_Full Stack Developer(Mern Stack).pdf";
+import pdf from "../../Assets/../Assets/Nidhi Parikh.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
